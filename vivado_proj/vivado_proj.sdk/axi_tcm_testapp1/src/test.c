0687516fba263dc2a88241ea874a029500f59c47
Henrique Ogawa <henrique.ogawa@usp.br>
2019-07-05 06:35:44 -0700

    adding macros and configuration functions

--- vivado_proj/vivado_proj.sdk/axi_tcm_testapp1/src/test.c
+++ vivado_proj/vivado_proj.sdk/axi_tcm_testapp1/src/test.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
-// DMA drivers
 #include "xaxidma.h"
 #include "xparameters.h"
 #include "xdebug.h"
-// MMIO drivers
 #include "xil_types.h"
 #include "xil_io.h"
+#include "command_gen.h"
 
+// DMA definitions
 #define DMA_DEV_ID      XPAR_AXIDMA_0_DEVICE_ID
 #define MEM_BASE_ADDR   0x01000000
 #define TX_BUFFER_BASE  (MEM_BASE_ADDR + 0x00100000)
-#define MAX_PKT_LEN	    0xC  // This is in bytes
+#define MAX_PKT_LEN     0xC  // This is in BYTES
 
-// DMA instance
+// DMA device instantiation
 XAxiDma AxiDma;
 XAxiDma_Config *CfgPtr;
-int Status;
+
+// axi_tcm commands
+#define AXI_TCM_CONTROL_REG(command)  Xil_Out32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR, command)
+#define STREAM_WRITE_EN               axi_tcm_command(0x0, TRUE, FALSE)
+#define READ_ADDR(addr)               axi_tcm_command(addr, FALSE, TRUE)
+#define AXI_TCM_DATA_READ             Xil_In32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR + 4)
 
 /**
- * \brief Initialize the XAxiDma device.
+ * \brief Initialize the XAxiDma device:
+ * - DMA in simple mode (no scatter-gather)
+ * - Disabled interrupts (polling mode)
  */
 long int AxiDma_Init() {
+	int Status;
+
 	CfgPtr = XAxiDma_LookupConfig(DMA_DEV_ID);
+
 	if (!CfgPtr) {
 		printf("No config found for %d\n", DMA_DEV_ID);
 		return XST_FAILURE;
@@ -38,17 +48,39 @@ long int AxiDma_Init() {
 		return XST_FAILURE;
 	}
 
-	// Disable interrupts, we use polling mode
 	XAxiDma_IntrDisable(&AxiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
 	return XST_SUCCESS;
 }
 
+/**
+ * \brief DMA transfer data:
+ * - Flush the SrcBuffer before the DMA transfer, in case the Data Cache is enabled
+ * - Sets up the DMA-To-Device so that data is sent
+ */
+int AxiDma_Tx(u32 *TxBufferPtr) {
+	int Status;
+
+	Xil_DCacheFlushRange((UINTPTR)TxBufferPtr, MAX_PKT_LEN);
+	Status = XAxiDma_SimpleTransfer(&AxiDma,(UINTPTR) TxBufferPtr, MAX_PKT_LEN, XAXIDMA_DMA_TO_DEVICE);
+
+	if (Status != XST_SUCCESS)
+		return XST_FAILURE;
+
+	return Status;
+}
+
+/**
+ * \brief Main function: demo
+ * - Transmit data from DMA to TCM through AXI-Stream bus
+ * - Read back from TCM through AXI-Lite control register
+ */
 int main() {
 	u32 *TxBufferPtr;
 	TxBufferPtr = (u32 *)TX_BUFFER_BASE;
+	u32 tcm_read;
 
 	// Configure tcm_control_reg: enable write process
-	Xil_Out32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR, 0x2);
+	AXI_TCM_CONTROL_REG(STREAM_WRITE_EN);
 
 	// Initialize and configure DMA
 	AxiDma_Init();
@@ -58,30 +90,21 @@ int main() {
 	TxBufferPtr[1] = 0xdeadbef0;
 	TxBufferPtr[2] = 0xdeadbef1;
 
-	// Flush the SrcBuffer before the DMA transfer, in case the Data Cache is enabled
-	// Sets up the DMA-To-Device so that data is sent
-	Xil_DCacheFlushRange((UINTPTR)TxBufferPtr, MAX_PKT_LEN);
-	Status = XAxiDma_SimpleTransfer(&AxiDma,(UINTPTR) TxBufferPtr, MAX_PKT_LEN, XAXIDMA_DMA_TO_DEVICE);
+	// Transmit data through DMA
+	AxiDma_Tx(TxBufferPtr);
 
-	if (Status != XST_SUCCESS)
-		return XST_FAILURE;
-
-	while (XAxiDma_Busy(&AxiDma,XAXIDMA_DMA_TO_DEVICE)) {/* wait... */}
+	while (XAxiDma_Busy(&AxiDma,XAXIDMA_DMA_TO_DEVICE)) {/* wait for TX */}
 	printf("Data sent... DONE\n");
 
-	u32 tcm_read;
 	// Configure tcm_control_reg: enable read process
-	Xil_Out32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR, 0x1);
-	tcm_read = Xil_In32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR + 4);
-	printf("Read from memory: %08x\n", tcm_read);
+	AXI_TCM_CONTROL_REG(READ_ADDR(0x00));
+	printf("Read from memory: %08x\n", AXI_TCM_DATA_READ);
 
-	Xil_Out32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR, 0x5);
-	tcm_read = Xil_In32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR + 4);
-	printf("Read from memory: %08x\n", tcm_read);
+	AXI_TCM_CONTROL_REG(READ_ADDR(0x01));
+	printf("Read from memory: %08x\n", AXI_TCM_DATA_READ);
 
-	Xil_Out32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR, 0x9);
-	tcm_read = Xil_In32(XPAR_TCM_AXI_TEST_0_S_AXIL_BASEADDR + 4);
-	printf("Read from memory: %08x\n", tcm_read);
+	AXI_TCM_CONTROL_REG(READ_ADDR(0x02));
+	printf("Read from memory: %08x\n", AXI_TCM_DATA_READ);
 
 	return XST_SUCCESS;
 }
